cac41c3bd9c24fc0d540e21a822ecbea54727e62
Thomas Pietrzak <thomas.pietrzak@gmail.com>
2022-04-27 23:29:10 +0200

    Oubli dans commit précédent

--- TactonDebug/AngleTester/angletester.cpp
+++ TactonDebug/AngleTester/angletester.cpp
@@ -1,56 +1,120 @@
-#include "angletester.h"
+#include "AngleTester.h"
 
 #include <cmath>
+#include <algorithm>
+using namespace std;
+
+#include <QLabel>
+#include <QPainter>
+#include <QVBoxLayout>
+#include <QFormLayout>
+#include <QPoint>
+
+#include "DMTactileDisplay.h"
 
 AngleTester::AngleTester(QWidget *parent, Qt::WindowFlags flags)
-	: QMainWindow(parent, flags)
+: QMainWindow(parent, flags), 
+  _angle(new DMTactileDisplay()), 
+  _frequency(new QSpinBox()), 
+  _tfrequency(new QSpinBox()), 
+  _play(new QPushButton()),
+  _stop(new QPushButton())
 {
-	try
-	{
-		//_tactonPlayer = new TactonPlayer("COM4");
-		_tactonPlayer = new TactonPlayer("/dev/tty.usbserial-FTFRHUAO"); // Mac USB serial cable
-	}
-	catch(...)
-	{
-		_tactonPlayer = NULL;
-	}
+  try
+  {
+    //_tactonPlayer = new TactonPlayer("COM4");
+    _tactonPlayer = new TactonPlayer("/dev/tty.usbserial-FTFRHUAO"); // Mac USB serial cable
+  }
+  catch(...)
+  {
+    _tactonPlayer = NULL;
+  }
 
-	setupUi(this);
+  //setupUi(this);
+  
+  resize(500, 600);
+  QWidget *centralWidget = new QWidget(this);
+  QVBoxLayout *verticalLayout = new QVBoxLayout(centralWidget);
+  verticalLayout->setSpacing(6);
+  verticalLayout->setContentsMargins(11, 11, 11, 11);
+  _angle->setMaximum(359);
+  _angle->setTracking(true);
+  _angle->setInvertedAppearance(false);
+  _angle->setInvertedControls(false);
+  _angle->setWrapping(true);
+  _angle->setNotchTarget(30.0);
+  //_angle->setNotchesVisible(true);
 
-	layout()->setSizeConstraint(QLayout::SetFixedSize);
+  verticalLayout->addWidget(_angle);
 
-	setFrequency(_frequency->value());
+  QFormLayout * formLayout = new QFormLayout();
+  formLayout->setSpacing(6);
 
-	connect(_play, SIGNAL(clicked(bool)), this, SLOT(play(bool)));
-	connect(_stop, SIGNAL(clicked(bool)), this, SLOT(stop(bool)));
+  QLabel *frequencyLabel = new QLabel(centralWidget);
+  frequencyLabel->setText("Background frequency");
+  _frequency->setMaximum(500);
+  _frequency->setValue(100);
+  formLayout->setWidget(0, QFormLayout::LabelRole, frequencyLabel);
+  formLayout->setWidget(0, QFormLayout::FieldRole, _frequency);
 
-	connect(_angle, SIGNAL(valueChanged(int)), this, SLOT(setAngle(int)));
-	connect(_frequency, SIGNAL(valueChanged(int)), this, SLOT(setFrequency(int)));
+  QLabel *tfrequencyLabel = new QLabel(centralWidget);
+  tfrequencyLabel->setText("Target frequency");
+  formLayout->setWidget(1, QFormLayout::LabelRole, tfrequencyLabel);
+  _tfrequency->setMaximum(500);
+  _tfrequency->setValue(250);
+  formLayout->setWidget(1, QFormLayout::FieldRole, _tfrequency);
+/*
+  _play->setText("Play");
+  _stop->setText("Stop");
+  formLayout->setWidget(2, QFormLayout::FieldRole, _play);
+  formLayout->setWidget(2, QFormLayout::LabelRole, _stop);
+*/
+  verticalLayout->addLayout(formLayout);
+  setCentralWidget(centralWidget);
+
+  //layout()->setSizeConstraint(QLayout::SetFixedSize);
+
+  _angle->addTarget(Target(10, 60));
+  _angle->addTarget(Target(100, 150));
+  _angle->addTarget(Target(200, 250));
+
+  setFrequency(_frequency->value());
+
+//  connect(_play, SIGNAL(clicked(bool)), this, SLOT(play(bool)));
+//  connect(_stop, SIGNAL(clicked(bool)), this, SLOT(stop(bool)));
+
+  //connect(_angle, SIGNAL(valueChanged(int)), this, SLOT(setAngle(int)));
+  connect(_angle, SIGNAL(updateAll(int, DMTactileDisplay::Hover)), this, SLOT(setOutput(int, DMTactileDisplay::Hover)));
+  connect(_angle, SIGNAL(stop()), this, SLOT(stop()));
+  connect(_frequency, SIGNAL(valueChanged(int)), this, SLOT(setFrequency(int)));
 }
 
 AngleTester::~AngleTester()
 {
-
 }
 
 void AngleTester::setFrequency(int frequency)
 {
-	_tactonPlayer->setFrequency(frequency);
+  _tactonPlayer->setFrequency(frequency);
 }
 
-void AngleTester::setAngle(int angle)
+void AngleTester::setOutput(int angle, DMTactileDisplay::Hover hover)
 {
-	_tactonPlayer->playAngle(angle);
-	return;
+  if (hover == DMTactileDisplay::OnTarget)
+    _tactonPlayer->setFrequency(_tfrequency->value());
+  else
+    _tactonPlayer->setFrequency(_frequency->value());
+  _tactonPlayer->playAngle(angle);
+  return;
 }
 
 void AngleTester::play(bool)
 {
-	setFrequency(_frequency->value());
-	setAngle(_angle->value());
+//  setFrequency(_frequency->value());
+//  setAngle(_angle->value());
 }
 
 void AngleTester::stop(bool)
 {
-	_tactonPlayer->stopBuzz();
+  _tactonPlayer->stopBuzz();
 }
